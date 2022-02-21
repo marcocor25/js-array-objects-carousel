6c1cbe3abacd7fc2d71b614ae3011adc4c49098c
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-21 17:25:08 +0100

    Creato l'array di oggetti

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
+// CREO L'ARRAY DI OGGETTI PER IL CAROUSEL
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
+    {
+        name: 'Svezia',
+        image: 'img/01.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    },
+    {
+        name: 'Svizzera',
+        image: 'img/02.jpg',
+        text: 'Lorem ipsum',
+    },
+    {
+        name: 'Gran Bretagna',
+        image: 'img/03.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+        name: 'Germania',
+        image: 'img/04.jpg',
+        text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
+    {
+        name: 'Paradise',
+        image: 'img/05.jpg',
+        text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    },
 ]
 
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-]
+console.log(items);
 
 // CREO LA VARIABILE COLLEGATA ALLO SLIDER-WRAPPER DELL'HTML
 const sliderWrapper = document.querySelector('.ms__slider_wrapper');
@@ -40,57 +47,57 @@ console.log(sliderImage);
 
 // ASSEGNO L'IMMAGINE AL TAG IMG
 slider.append(sliderImage);
-sliderImage.src = items[1];
+sliderImage.src = null;
 
 // CREO LA VARIABILE COLLEGATA ALL'ITEM-WRAPPER DELL'HTML
 const itemWrapper = document.querySelector('.ms__item_wrapper');
 console.log(itemWrapper);
 
-// VARIABILE CURRENT INDEX
-let currentIndex = 0;
+// // VARIABILE CURRENT INDEX
+// let currentIndex = 0;
 
-// CREO IL CICLO PER GENERARE I DIV DELLE PREVIEW
-for (let i = 0; i < items.length ; i++) {
-    const itemBox = document.createElement('div');
-    console.log(itemBox);
+// // CREO IL CICLO PER GENERARE I DIV DELLE PREVIEW
+// for (let i = 0; i < items.length ; i++) {
+//     const itemBox = document.createElement('div');
+//     console.log(itemBox);
 
-    itemWrapper.append(itemBox);
-    itemBox.classList.add('item');
+//     itemWrapper.append(itemBox);
+//     itemBox.classList.add('item');
     
-    const itemImage = document.createElement('img');
-    console.log(itemImage);
+//     const itemImage = document.createElement('img');
+//     console.log(itemImage);
 
-    itemBox.append(itemImage);
-    itemImage.src = items[i];
+//     itemBox.append(itemImage);
+//     itemImage.src = items[i];
 
-    if (currentIndex == i ) {
-        itemBox.classList.add('active_bis');
-    }
-}
+//     if (currentIndex == i ) {
+//         itemBox.classList.add('active_bis');
+//     }
+// }
 
-// CREO VARIABILE PER RAGGRUPPARE TUTTE LE CLASSI DELLE IMMAGINI
-let allImages = document.querySelectorAll('.item');
-console.log(allImages);
+// // CREO VARIABILE PER RAGGRUPPARE TUTTE LE CLASSI DELLE IMMAGINI
+// let allImages = document.querySelectorAll('.item');
+// console.log(allImages);
 
-// CREO LA VARIABILE COLLEGATA ALLA CHEVRON-UP DELL'HTML
-const prevButton = document.getElementById('chevron-up');
-console.log(prevButton);
+// // CREO LA VARIABILE COLLEGATA ALLA CHEVRON-UP DELL'HTML
+// const prevButton = document.getElementById('chevron-up');
+// console.log(prevButton);
 
-prevButton.addEventListener('click', function() {
-    console.log('It works!');
-})
+// prevButton.addEventListener('click', function() {
+//     console.log('It works!');
+// })
 
-// CREO LA VARIABILE COLLEGATA ALLA CHEVRON-DOWN DELL'HTML
-const nextButton = document.getElementById('chevron-down');
-console.log(nextButton);
+// // CREO LA VARIABILE COLLEGATA ALLA CHEVRON-DOWN DELL'HTML
+// const nextButton = document.getElementById('chevron-down');
+// console.log(nextButton);
 
-// CREAO AZIONE AZIONE AL CLICK DELLA CHEVRON-DOWN
-nextButton.addEventListener('click', function() {
-    console.log('And it works as well!');
+// // CREAO AZIONE AZIONE AL CLICK DELLA CHEVRON-DOWN
+// nextButton.addEventListener('click', function() {
+//     console.log('And it works as well!');
 
-    if (currentIndex < 4 ) {
-        allImages[currentIndex].classList.remove('active_bis');
-        currentIndex++
-        allImages[currentIndex].classList.add('active_bis');
-    }
-})
+//     if (currentIndex < 4 ) {
+//         allImages[currentIndex].classList.remove('active_bis');
+//         currentIndex++
+//         allImages[currentIndex].classList.add('active_bis');
+//     }
+// })
